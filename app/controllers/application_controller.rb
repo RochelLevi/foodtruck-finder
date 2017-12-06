caf801c57b0f701802d9534848ce8bb58aec1cce
RochelLevi <rochlevi@gmail.com>
2017-12-06 09:41:57 -0500

    Adds minor changes after merging with priyam

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # before_action :authorized
+  before_action :authorized
 
   helper_method :logged_in?
   helper_method :current_user
